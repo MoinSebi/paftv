a400d5e5abd3a4b2be81c6b3ee56405e19f23364
moinSebi <s.vorbrugg@googlemail.com>
2021-03-17 19:27:03 +0100

    subsetting now looks at files names - reduce number

--- subset_paf.py
+++ subset_paf.py
@@ -9,14 +9,15 @@ Created on 3/17/21
 
 import sys
 
-def readPaf(paf_file, header_pos):
+def readPaf(paf_file, header_pos, k):
     paf_file_new = []
 
     with open(paf_file) as f:
         for line in f.readlines():
             lsplit = line.split()
             if lsplit[0] in header_pos and lsplit[5] in header_pos:
-                paf_file_new.append(line)
+                if k[lsplit[0]] != k[lsplit[5]]:
+                    paf_file_new.append(line)
     for x in paf_file_new:
         print(x.replace("\n", ""))
 
@@ -25,13 +26,15 @@ def readPaf(paf_file, header_pos):
 
 def readFasta(fasta_file):
     entries = []
+    k = dict()
     for x in fasta_file:
         with open(x) as f:
             for line in f.readlines():
                 if line.startswith(">"):
                     entries.append(line[1:].replace("\n", ""))
-    return  set(entries)
+                    k[line[1:].replace("\n", "")] = x
+    return  set(entries), k
 
 if __name__ == "__main__":
-    j = readFasta(sys.argv[2:])
+    j, k = readFasta(sys.argv[2:])
     readPaf(sys.argv[1], j)
